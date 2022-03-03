b8e18b42d10593cd6ff0aa7526126691547ac0e4
James N. Davis - C212771 <48291969+jdavis-EliLilly@users.noreply.github.com>
2022-03-03 08:56:38 -0500

    Update Two_Sum.py

--- Interview_Questions/Two_Sum/Two_Sum.py
+++ Interview_Questions/Two_Sum/Two_Sum.py
@@ -1,3 +1,10 @@
+   #Problem Statement
+    
+    '''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.'''
+    
+    
+    
     # List to store results
     result = []
     # Dictionary to store the difference and its index
