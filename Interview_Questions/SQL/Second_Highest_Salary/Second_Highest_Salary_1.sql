5205dcccff9b9c113e9467d1c033468026acc947
James N. Davis - C212771 <48291969+jdavis-EliLilly@users.noreply.github.com>
2022-04-26 09:44:42 -0400

    Update Second_Highest_Salary_1.sql
    Solution
    Runtime: 296 ms, faster than 19.04% of MySQL online submissions for Second Highest Salary.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Second Highest Salary.

--- Interview_Questions/SQL/Second_Highest_Salary/Second_Highest_Salary_1.sql
+++ Interview_Questions/SQL/Second_Highest_Salary/Second_Highest_Salary_1.sql
@@ -1,7 +1,21 @@
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 Select 
 e.salary as SecondHighestSalary
 from
 employee e
 --ORDER BY e.salary desc limit n-1,1
 ORDER BY e.salary desc limit 1,1
+
+--working Solution
+select(
+    select 
+        salary 
+    from 
+        employee 
+    group by 
+        salary
+    order by
+        salary
+    desc 
+    limit 1,1
+)as SecondHighestSalary
