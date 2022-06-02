dca311cd888efe38c1ac177ecdc07eedf3d26e3e
James N. Davis - C212771 <48291969+jdavis-EliLilly@users.noreply.github.com>
2022-06-02 10:44:10 -0400

    Update Customer_Refree.sql

--- Interview_Questions/SQL/Customer_not_2/Customer_Refree.sql
+++ Interview_Questions/SQL/Customer_not_2/Customer_Refree.sql
@@ -1,4 +1,6 @@
-SELECT name FROM Customer WHERE(referee_id!=2 OR referee_id IS NULL);
+SELECT name
+FROM Custome
+r WHERE(referee_id!=2 OR referee_id IS NULL);
 
 --Runtime: 578 ms, faster than 27.34% of MySQL online submissions for Find Customer Referee.
 --Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Customer Referee.
@@ -9,4 +11,11 @@ from Customer
 Where referee_id != "2" or referee_id is Null
 
 --Runtime: 557 ms, faster than 32.85% of MySQL online submissions for Find Customer Referee.
+--Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Customer Referee.
+
+SELECT name 
+FROM Customer
+WHERE COALESCE(referee_id, 0) != 2;
+
+--Runtime: 543 ms, faster than 37.24% of MySQL online submissions for Find Customer Referee.
 --Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Customer Referee.
