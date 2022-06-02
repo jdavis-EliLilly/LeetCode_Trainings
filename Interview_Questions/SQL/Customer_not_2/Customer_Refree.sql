84d180d1e3f02d8d71cfe25ad39317a4c6543a81
James N. Davis - C212771 <48291969+jdavis-EliLilly@users.noreply.github.com>
2022-06-02 10:44:39 -0400

    Formatting change

--- Interview_Questions/SQL/Customer_not_2/Customer_Refree.sql
+++ Interview_Questions/SQL/Customer_not_2/Customer_Refree.sql
@@ -1,6 +1,6 @@
 SELECT name
-FROM Custome
-r WHERE(referee_id!=2 OR referee_id IS NULL);
+FROM Customer
+WHERE(referee_id!=2 OR referee_id IS NULL);
 
 --Runtime: 578 ms, faster than 27.34% of MySQL online submissions for Find Customer Referee.
 --Memory Usage: 0B, less than 100.00% of MySQL online submissions for Find Customer Referee.
